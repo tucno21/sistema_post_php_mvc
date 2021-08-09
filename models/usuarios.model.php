797a2270865baa7b9e1e4555e7f9dea22d50df01
tucno21 <carlitostucno@gmail.com>
2021-08-09 17:07:41 -0500

    modelo ususario

---
+++ models/usuarios.model.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+class ModeloUsuarios
+{
+    //recibe los datos de controller
+    public static function MostrarUsuario($table, $colum, $valorColum)
+    {
+        //codigos de MYSQL PDO para traer solo el ususario // :$colum para comparar
+        //$stmt Sentencias Preparadas en PDO
+        $stmt = Conexion::conectar()->prepare("SELECT * FROM $table WHERE $colum = :$colum");
+        //comparar y que solo sera texto (PDO::PARAM_STR)
+        $stmt->bindParam(":" . $colum, $valorColum, PDO::PARAM_STR);
+        //ejecutar la linea de comando
+        $stmt->execute();
+
+        //enviar al controlador usuario
+        return $stmt->fetch();
+    }
+}
