89f83ec2c9eea3cd84a31801098030ea6303e0ab
tucno21 <carlitostucno@gmail.com>
2021-08-09 17:01:08 -0500

    controlador para consultar el modeloUsuario

---
+++ controllers/usuarios.controller.php
@@ -0,0 +1,37 @@
+<?php
+
+class ControllerUsuarios
+{
+    public function ingresoUsuario()
+    {
+        // recibe el nombre de usario por post
+        if (isset($_POST["usuario"])) {
+            //validar que solo sea letras y numeros
+            if (
+                preg_match('/^[a-zA-z0-9]+$/', $_POST["usuario"]) &&
+                preg_match('/^[a-zA-z0-9]+$/', $_POST["password"])
+            ) {
+                //variable para las consultas
+                $table = "usuarios";
+                $colum =  "usuario";
+                $valorColum = $_POST["usuario"];
+
+                //conectar y recibir una respuesta del MODEL
+                //trae la fila del usuario que estoy buscando...
+                $respuesta = ModeloUsuarios::MostrarUsuario($table, $colum, $valorColum);
+
+                //comparar el ingreso con la tabla
+                if ($respuesta["usuario"] == $_POST["usuario"] && $respuesta["password"] == $_POST["password"]) {
+
+                    //crear variable de la sesion
+                    $_SESSION["inicioSession"] = "ok";
+                    //luego enviar a pagina inicio
+                    echo '<script>window.location="inicio"</script>';
+                } else {
+                    echo '<div class="alert alert-danger">error al ingresar intente de nuevo</div>';
+                }
+                // var_dump($respuesta["password"]);
+            }
+        }
+    }
+}
--- views/modules/login.php
+++ views/modules/login.php
@@ -8,9 +8,9 @@
             <div class="card-body login-card-body">
                 <p class="login-box-msg">Ingresar al sistema</p>
 
-                <form action="../../index3.html" method="post">
+                <form method="post">
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email" name="email" required>
+                        <input type="text" class="form-control" placeholder="Email" name="usuario" required>
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
@@ -33,6 +33,12 @@
                         </div>
                         <!-- /.col -->
                     </div>
+
+                    <?php
+                    $login = new ControllerUsuarios();
+                    $login->ingresoUsuario();
+                    ?>
+
                 </form>
 
             </div>
