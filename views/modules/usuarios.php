d614cad5c397bbd9ccc883da2e8cf5bc3c2e48db
tucno21 <carlitostucno@gmail.com>
2021-08-11 18:31:03 -0500

    agregado modal con formulario

--- views/modules/usuarios.php
+++ views/modules/usuarios.php
@@ -63,3 +63,80 @@
     <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
+
+<!-- Modal -->
+<div class="modal fade" id="modalAgregarUsuario" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form role="form" method="post" action="" enctype="multipart/form-data">
+                <div class="modal-header" style="background:#343a40; color:#fff">
+                    <h5 class="modal-title">Agregar Usuario</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" style="color:#fff">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="box-body">
+                        <!-- NOMBRE -->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <spam class="input-group-text"><i class="fa fa-user"></i></spam>
+                                </div>
+                                <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Ingresar Nombre" required>
+                            </div>
+                        </div>
+                        <!-- USUARIO -->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <spam class="input-group-text"><i class="fa fa-key"></i></spam>
+                                </div>
+                                <input type="text" class="form-control input-lg" name="nuevoUsiario" placeholder="Ingresar Usuario" required>
+                            </div>
+                        </div>
+                        <!-- contraseña -->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <spam class="input-group-text"><i class="fa fa-lock"></i></spam>
+                                </div>
+                                <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder="Ingresar Contraseña" required>
+                            </div>
+                        </div>
+                        <!-- Perfil -->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <spam class="input-group-text"><i class="fa fa-users"></i></spam>
+                                </div>
+                                <select class="form-control input-lg" name="nuevoPerfil">
+                                    <option value="">Seleccione Perfil</option>
+                                    <option value="Administrador">Administrador</option>
+                                    <option value="Especial">Especial</option>
+                                    <option value="Vendedor">Vendedor</option>
+                                </select>
+                            </div>
+                        </div>
+                        <!-- foto -->
+                        <div class="form-group">
+
+                            <div class="border border-secondary">
+                                <p CLASS="text-center">SUBIR FOTO</p>
+
+                                <input type="file" class="form-control input-lg" name="nuevoFoto" id="nuevoFoto">
+                                <p class=help-block>Peso máximo de 1mb</p>
+                                <img src="views/dist/img/avatar.png" alt="avatar" class="img-thumbnail" width="70px">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-primary">Agregar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
