1811ae7b3d455caa217b314632260d8adf0db611
tucno21 <carlitostucno@gmail.com>
2021-08-13 14:47:48 -0500

    datatable responsive

--- views/modules/usuarios.php
+++ views/modules/usuarios.php
@@ -22,7 +22,7 @@
             </div>
 
             <div class="card-body">
-                <table class="table table-bordered table-striped dataTable dtr-inline tablas">
+                <table class="table table-bordered table-striped table-responsive dtr-inline tablas">
                     <thead>
                         <tr>
                             <th>N°</th>
